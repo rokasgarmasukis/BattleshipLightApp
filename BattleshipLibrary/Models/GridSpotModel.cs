bdfb4a699d2319073db06930baa2602bacf6b4eb
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-05-09 15:30:10 +0200

    Add project files.

---
+++ BattleshipLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary.Models
+{
+    public class GridSpotModel
+    {
+        public string SpotLetter { get; set; }
+        public int SpotNumber { get; set; }
+    }
+}
---
+++ BattleshipLibrary/Models/PlayerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary.Models
+{
+    public class PlayerModel
+    {
+        public string Username { get; set; }
+        public List<GridSpotModel> ShipLocations { get; set; }
+        public List<GridSpotModel> ShotGrid { get; set; }
+    }
+}
